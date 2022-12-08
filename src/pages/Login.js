11112d05211bc4a13bf46c6fecf873745cfc49cd
Bruno Ferreira <contatobrunokye@gmail.com>
2022-12-08 20:48:17 -0300

    Adiciona requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../styles/login.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { loginSuccess } from '../redux/actions';
 
-class Login extends React.Component {
+class Login extends Component {
+  state = {
+    btnDisabled: true,
+    email: '',
+  };
+
+  handleChange = ({ target }) => {
+    const inputEmail = document.getElementById('input-email');
+    const inputPassword = document.getElementById('input-password');
+
+    // RegEx -> https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+    const regEx = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const MIN_PASSWORD_SIZE = 6;
+    const loginValidation = regEx.test(inputEmail.value)
+      && inputPassword.value.length >= MIN_PASSWORD_SIZE
+      ? this.setState({ btnDisabled: false })
+      : this.setState({ btnDisabled: true });
+
+    const { name, value } = target;
+    this.setState({ [name]: value });
+
+    return loginValidation;
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+
+    dispatch(loginSuccess(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { btnDisabled } = this.state;
+    const { handleChange, handleSubmit } = this;
+
+    return (
+      <div>
+        <form className="form-container">
+          <input
+            id="input-email"
+            data-testid="email-input"
+            type="text"
+            name="email"
+            placeholder="Digite seu email"
+            onChange={ handleChange }
+          />
+
+          <input
+            id="input-password"
+            data-testid="password-input"
+            type="text"
+            name="password"
+            placeholder="Digite sua senha"
+            onChange={ handleChange }
+          />
+
+          <button
+            type="submit"
+            disabled={ btnDisabled }
+            onClick={ handleSubmit }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,16 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN_SUCCESS } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case LOGIN_SUCCESS:
+    return { email: action.payload };
+  default:
+    return state;
+  }
+}
+
+export default user;
